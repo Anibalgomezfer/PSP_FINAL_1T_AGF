6d8360a5c6c5d700823799294bab77495a3f9298
Anibalgomezfer <59883735+Anibalgomezfer@users.noreply.github.com>
2020-11-26 16:05:59 +0100

    Primera parte de la práctica, incluido el jar

---
+++ src/main/java/com/clases/PSP_FINAL_1T_AGF/Main.java
@@ -0,0 +1,66 @@
+package com.clases.PSP_FINAL_1T_AGF;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int numRegistros = 0;
+        int numHilos = 0;
+        int numRegistrosPorHilo;
+
+        ArrayList<Empleado> arrayEmpleados = new ArrayList<>();
+        Scanner teclado = new Scanner(System.in);
+
+
+        try {
+            while (numRegistros <= 0) {
+                System.out.println("Introduce el número de registros que quieres introducir: ");
+                numRegistros = teclado.nextInt();
+                if (numRegistros <= 0) {
+                    System.out.println("Debes introducir al menos 1 registro.");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Parece que has introducido una letra y se esperaba un número.");
+            System.out.println("Inicia el programa de nuevo.");
+
+        }
+
+
+        try {
+            while (numHilos <= 0) {
+                System.out.println("Introduce el número de hilos que deseas utilizar: ");
+                numHilos = teclado.nextInt();
+                if (numHilos <= 0) {
+                    System.out.println("Debes utilizar al menos un hilo.");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Parece que has introducido una letra y se esperaba un número.");
+            System.out.println("Inicia el programa de nuevo.");
+        }
+
+        int modulo = (numRegistros % numHilos);
+        numRegistrosPorHilo = numRegistros / numHilos;
+
+        for (int indice = 0; indice < numHilos; indice++) {
+
+            if (indice == (numHilos-1)){
+                numRegistrosPorHilo = numRegistrosPorHilo+modulo;
+                Empleado hilo = new Empleado(numRegistros, numHilos, numRegistrosPorHilo, indice);
+                arrayEmpleados.add(hilo);
+            }else{
+                Empleado hilo = new Empleado(numRegistros, numHilos, numRegistrosPorHilo, indice);
+                arrayEmpleados.add(hilo);
+            }
+
+        }
+        for (int j = 0; j < arrayEmpleados.size(); j++) {
+            arrayEmpleados.get(j).start();
+        }
+    }
+
+
+}
